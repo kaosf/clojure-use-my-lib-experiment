a4c3cc059e404bae52f3b5c7d12e11c0ed2fd729
ka <ka.kaosf@gmail.com>
2013-12-31 02:09:31 +0900

    add (:require ...) into (ns ...)
    but comment out it

--- src/clojure_use_my_lib_experiment/core.clj
+++ src/clojure_use_my_lib_experiment/core.clj
@@ -1,5 +1,7 @@
 (ns clojure-use-my-lib-experiment.core
-  (:gen-class))
+  (:gen-class)
+  ;(:require [clojure-library-experiment.core :refer :all]))
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
