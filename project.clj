3ea17819368c756f59f0f62a9f0bf544a816f301
ka <ka.kaosf@gmail.com>
2013-12-31 01:41:44 +0900

    modify project.clj to use "clojure-library-experiment"

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clojure-library-experiment/clojure-library-experiment "0.1.0-SNAPSHOT"]]
   :main ^:skip-aot clojure-use-my-lib-experiment.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
